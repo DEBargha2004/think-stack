8f3065a9d6cdd22a440856986557da3578a6b7f8
DEBargha2004 <debarghasaha16@gmail.com>
2024-10-17 14:56:40 +0530

    feat
    css and sql highlighting added

--- src/components/custom/idea-content.tsx
+++ src/components/custom/idea-content.tsx
@@ -3,19 +3,24 @@
 import { HTMLProps, useEffect } from "react";
 import { TContent } from "../../../schema/idea";
 import { cn } from "@/lib/utils";
+import CodeEditorWrapper from "./code-editor-wrapper";
 
 import hljs from "highlight.js/lib/core";
 import javascript from "highlight.js/lib/languages/javascript";
 import python from "highlight.js/lib/languages/python";
 import cpp from "highlight.js/lib/languages/cpp";
 import html from "highlight.js/lib/languages/xml";
+import sql from "highlight.js/lib/languages/sql";
+import css from "highlight.js/lib/languages/css";
+
 import "highlight.js/styles/atom-one-dark.css";
-import CodeEditorWrapper from "./code-editor-wrapper";
 
 hljs.registerLanguage("javascript", javascript);
 hljs.registerLanguage("python", python);
 hljs.registerLanguage("cpp", cpp);
 hljs.registerLanguage("html", html);
+hljs.registerLanguage("sql", sql);
+hljs.registerLanguage("css", css);
 
 export default function IdeaContent({
   ideaContent,
